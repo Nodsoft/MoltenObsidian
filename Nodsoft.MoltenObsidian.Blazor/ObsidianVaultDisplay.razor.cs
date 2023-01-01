7f61b603d680b32da629f7c7a38f8eecd0011057
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2023-01-01 19:39:45 +0100

    Add Display options for Blazor + Refactor default render templates

--- Nodsoft.MoltenObsidian.Blazor/ObsidianVaultDisplay.razor.cs
+++ Nodsoft.MoltenObsidian.Blazor/ObsidianVaultDisplay.razor.cs
@@ -1,6 +1,7 @@
 ﻿using JetBrains.Annotations;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using Nodsoft.MoltenObsidian.Blazor.Helpers;
 using Nodsoft.MoltenObsidian.Blazor.Services;
 using Nodsoft.MoltenObsidian.Vault;
 
@@ -12,19 +13,85 @@ namespace Nodsoft.MoltenObsidian.Blazor;
 [PublicAPI]
 public sealed partial class ObsidianVaultDisplay : ComponentBase
 {
+	/// <summary>
+	/// The Vault being displayed.
+	/// </summary>
 	[Inject, Parameter] public IVault Vault { get; set; } = null!;
+	
+	/// <summary>
+	/// The router used to navigate the vault.
+	/// </summary>
 	[Inject] public VaultRouter Router { get; set; } = null!;
+	
+	/// <summary>
+	/// Blazor Navigation manager to navigate to other pages.
+	/// </summary>
 	[Inject] public NavigationManager Navigation { get; set; } = null!;
+	
+	/// <summary>
+	/// Javascript runtime to interact with the browser.
+	/// </summary>
 	[Inject] public IJSRuntime Js { get; set; } = null!;
 
+	
+	/// <summary>
+	/// The current path being navigated.
+	/// </summary>
+	/// <remarks>
+	/// This is usually bound from the slug parameter of the blazor page hosting this component.
+	/// </remarks>
 	[Parameter, EditorRequired] public string? CurrentPath { get; set; } = "/";
+	
+	/// <summary>
+	/// The base path from which the vault is being served.
+	/// </summary>
+	/// <remarks>
+	/// This is usually bound from the base parameter of the blazor page hosting this component.
+	/// In most cases, it can be resolved using <see cref="VaultComponentHelpers.GetCallingBaseVaultPath"/>.
+	/// </remarks>
 	[Parameter, EditorRequired] public string? BasePath { get; set; } = "/";
+	
+	/// <summary>
+	/// The options to use for rendering the vault.
+	/// </summary>
+	[Parameter] public ObsidianVaultDisplayOptions Options { get; set; } = new();
 
-	[Parameter] public RenderFragment<IVault> Index { get; set; } = DefaultTemplates.IndexDefaultTemplate;
-	[Parameter] public RenderFragment<string> NotFound { get; set; } = DefaultTemplates.NotFoundDefaultTemplate;
-	[Parameter] public RenderFragment<IVaultNote> FoundNote { get; set; } = DefaultTemplates.FoundNoteDefaultTemplate;
-	[Parameter] public RenderFragment<IVaultFolder> FoundFolder { get; set; } = DefaultTemplates.FoundFolderDefaultTemplate;
-	[Parameter] public RenderFragment<(string, Exception)> Error { get; set; } = DefaultTemplates.ErrorDefaultTemplate;
+	/// <summary>
+	/// The render fragment in charge of rendering the vault's index.
+	/// </summary>
+	/// <value>
+	///	By default, this is set to <see cref="Templates.FoundVaultIndex"/>, which renders a list of all entities in the vault.
+	/// </value>
+	/// <seealso cref="IVault"/>
+	[Parameter] public RenderFragment<IVault> Index { get; set; } = Templates.FoundVaultIndex.Render;
+	
+	/// <summary>
+	/// The render fragment used when nothing is found at the current path.
+	/// </summary>
+	[Parameter] public RenderFragment<string> NotFound { get; set; } = Templates.NotFound.Render;
+	
+	/// <summary>
+	/// The render fragment used when hitting a Note on the current path.
+	/// </summary>
+	/// <value>
+	/// By default, this is set to <see cref="DefaultTemplates.FoundNoteDefaultTemplate"/>, which renders the note's content.
+	/// </value>
+	/// <seealso cref="IVaultNote"/>
+	[Parameter] public RenderFragment<IVaultNote> FoundNote { get; set; } = Templates.FoundNote.Render;
+	
+	/// <summary>
+	/// The render fragment used when hitting a Folder on the current path.
+	/// </summary>
+	/// <value>
+	/// By default, this is set to <see cref="DefaultTemplates.FoundFolderDefaultTemplate"/>, which renders a list of all immediate descending entities in the folder.
+	/// </value>
+	/// <seealso cref="IVaultFolder"/>
+	[Parameter] public RenderFragment<IVaultFolder> FoundFolder { get; set; } = Templates.FoundFolder.Render;
+	
+	/// <summary>
+	/// The render fragment used when an error occurs.
+	/// </summary>
+	[Parameter] public RenderFragment<(string, Exception)> Error { get; set; } = Templates.Error.Render;
 
 
 	private IVaultEntity? _foundEntity;
@@ -44,6 +111,9 @@ public sealed partial class ObsidianVaultDisplay : ComponentBase
 
 	protected override async Task OnAfterRenderAsync(bool firstRender)
 	{
-		await Js.InvokeVoidAsync("eval", /*lang=javascript*/$"document.querySelector('base').href = '{BasePath}'");
+		if (firstRender)
+		{
+			await Js.InvokeVoidAsync("eval", /*lang=javascript*/$"document.querySelector('base').href = '{BasePath}'");
+		}
 	}
 }
---
+++ Nodsoft.MoltenObsidian.Blazor/ObsidianVaultDisplayOptions.cs
@@ -0,0 +1,31 @@
+﻿namespace Nodsoft.MoltenObsidian.Blazor;
+
+/// <summary>
+/// Defines options for the <see cref="ObsidianVaultDisplay" />.
+/// </summary>
+/// <seealso cref="ObsidianVaultDisplay" />
+public readonly struct ObsidianVaultDisplayOptions
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ObsidianVaultDisplayOptions" /> class.
+	/// </summary>
+	public ObsidianVaultDisplayOptions() { }
+	
+	/// <summary>
+	/// If enabled, any <c>README.md</c> or <c>index.md</c> file will be displayed as the default/index page.
+	/// </summary>
+	/// <remarks>
+	/// Defaults to <see langword="true" />.
+	/// </remarks>
+	public bool DisplayReadmeOnFolderRoot { get; init; } = true;
+	
+	/// <summary>
+	/// If enabled, a folder index note display will also render the navigation tree for the current folder.
+	/// This setting can be individually overriden by the <c>moltenobsidian:index:navigation</c> front matter property.
+	/// </summary>
+	/// <remarks>
+	/// Defaults to <see langword="true" />.
+	/// </remarks>
+	/// <seealso cref="ObsidianVaultDisplayOptions.DisplayReadmeOnFolderRoot" />
+	public bool DisplayFolderNavigation { get; init; } = true;
+}
