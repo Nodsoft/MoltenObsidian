167a146f40361a46c289c119763a3488673e224d
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2023-08-05 13:13:08 +0200

    refactor(manifest): refactor VaultManifestGenerator
    - Added a project reference to the manifest project in the csproj file.
    - Refactored VaultManifestGenerator to use IVault instead of FileSystemVault.
    - Updated GenerateManifestAsync method to handle different versions of .NET.
    - Removed unused namespaces from VaultManifestGenerator.
    - Removed unused namespace from GenerateManifestCommand.

--- Nodsoft.MoltenObsidian.Manifest/VaultManifestGenerator.cs
+++ Nodsoft.MoltenObsidian.Manifest/VaultManifestGenerator.cs
@@ -1,9 +1,7 @@
 ﻿using System.Security.Cryptography;
 using Nodsoft.MoltenObsidian.Vault;
-using Nodsoft.MoltenObsidian.Vaults.FileSystem;
-using Nodsoft.MoltenObsidian.Manifest;
 
-namespace Nodsoft.MoltenObsidian.Tool.Services;
+namespace Nodsoft.MoltenObsidian.Manifest;
 
 /// <summary>
 /// Provides manifest generation for a filesystem-based Molten Obsidian vault.
@@ -11,11 +9,11 @@ namespace Nodsoft.MoltenObsidian.Tool.Services;
 public static class VaultManifestGenerator
 {
 	/// <summary>
-	/// Generates a manifest for the specified filesystem vault.
+	/// Generates a manifest for the specified vault.
 	/// </summary>
 	/// <param name="vault">The path to the vault.</param>
 	/// <returns>A <see cref="RemoteVaultManifest"/> instance.</returns>
-	public static async Task<RemoteVaultManifest> GenerateManifestAsync(FileSystemVault vault)
+	public static async Task<RemoteVaultManifest> GenerateManifestAsync(IVault vault)
 	{
 		List<ManifestFile> files = new();
 
@@ -46,11 +44,27 @@ public static class VaultManifestGenerator
 		// Read the file. We'll need it later.
 		await using Stream stream = await file.OpenReadAsync();
 
+#if !NET7_0_OR_GREATER
+		byte[] fileBytes = new byte[stream.Length];
+		int readBytesCount = await stream.ReadAsync(fileBytes);
+		
+		if (readBytesCount != stream.Length)
+		{
+			throw new InvalidOperationException("The file could not be read.");
+		}
+#endif
+
 		// Build a new file object
 		return new()
 		{
 			Path = file.Path,
-			Hash = Convert.ToBase64String(await SHA256.HashDataAsync(stream)), // Create a SHA256 hash of the file
+			
+#if NET7_0_OR_GREATER
+			Hash = Convert.ToBase64String(await SHA256.HashDataAsync(stream)), // Create a SHA256 hash of the file 
+#else
+			Hash = Convert.ToBase64String(SHA256.HashData(fileBytes)), // Create a SHA256 hash of the file
+#endif
+			
 			Size = stream.Length,
 			ContentType = Manifest.MimeTypes.GetMimeType(file.Name), // A fuller namespace is required here because of a conflict with the other apparitions of MimeTypes.
 			Metadata = file is IVaultNote note ? (await note.ReadDocumentAsync()).Frontmatter : new()
