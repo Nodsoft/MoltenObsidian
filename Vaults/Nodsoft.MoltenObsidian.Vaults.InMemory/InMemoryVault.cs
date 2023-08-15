63c50460a40ccd0f9f9368229a2be96bf83b5234
Spencer Naugler <spencernaugler7@gmail.com>
2023-08-14 23:16:45 -0400

    Incomplete commit need changes for inmemory

---
+++ Vaults/Nodsoft.MoltenObsidian.Vaults.InMemory/InMemoryVault.cs
@@ -0,0 +1,68 @@
+﻿using JetBrains.Annotations;
+using Microsoft.Extensions.Caching.Memory;
+using Nodsoft.MoltenObsidian.Utilities;
+using Nodsoft.MoltenObsidian.Vault;
+using Nodsoft.MoltenObsidian.Vaults.InMemory.Data;
+
+namespace Nodsoft.MoltenObsidian.Vaults.InMemory;
+
+/// <summary>
+/// 
+/// </summary>
+public sealed class InMemoryVault: IVault
+{
+
+    private InMemoryVault(){}
+
+    private MemoryCache Cache { get; set; }
+    public string Name { get; private set; } = null!;
+    public IVaultFolder Root { get; private set; }
+    public IReadOnlyDictionary<string, IVaultFile> Files { get; private set; }
+    public IReadOnlyDictionary<string, IVaultFolder> Folders { get; private set; }
+    
+    public IReadOnlyDictionary<string, IVaultNote> Notes { get; private set; }
+    public static IEnumerable<string> DefaultIgnoredFolders { get; } = new[] { ".obsidian", ".git", ".vs", ".vscode", "node_modules" };
+    public static IEnumerable<string> DefaultIgnoredFiles { get; } = new[] { ".DS_Store" };
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="directoryInfo"></param>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    [PublicAPI]
+    public static InMemoryVault FromDirectory(MemoryCache cache, DirectoryInfo directoryInfo) =>
+        FromDirectory(directoryInfo, DefaultIgnoredFolders, DefaultIgnoredFiles, cache);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="directory"></param>
+    /// <param name="ignoredFolders"></param>
+    /// <param name="ignoredFiles"></param>
+    /// <param name="cache"></param>
+    /// <returns></returns>
+    /// <exception cref="DirectoryNotFoundException"></exception>
+    [PublicAPI]
+    public static InMemoryVault FromDirectory(DirectoryInfo directory,
+        IEnumerable<string> ignoredFolders,
+        IEnumerable<string> ignoredFiles, 
+        MemoryCache cache)
+    {
+        if (!directory.Exists)
+        {
+            throw new DirectoryNotFoundException("The specified directory does not exist.");
+        }
+
+        InMemoryVault vault = new()
+        {
+            Cache = cache
+        };
+
+        vault.Root = new InMemoryVaultFolder(directory, null, vault);
+        vault.Name = directory.Name;
+        vault.Folders = new Dictionary<string, IVaultFolder>(
+            vault.Root.GetFolders(SearchOption.AllDirectories));
+    }
+    
+}
---
+++ Vaults/Nodsoft.MoltenObsidian.Vaults.InMemory/VaultDependencyInjectionExtensions.cs
@@ -0,0 +1,67 @@
+﻿using JetBrains.Annotations;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+using Nodsoft.MoltenObsidian.Vault;
+using Nodsoft.MoltenObsidian.Vaults.InMemory;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+[PublicAPI]
+public static class VaultDependencyInjectionExtensions
+{
+
+	private static readonly MemoryCacheOptions DefaultCacheOptions = new MemoryCacheOptions();
+
+	private static readonly MemoryCacheEntryOptions DefaultEntryCacheOptions = new MemoryCacheEntryOptions()
+		.AddExpirationToken(new CancellationChangeToken(CancellationToken.None))
+		.SetSlidingExpiration(TimeSpan.FromSeconds(5));
+
+	/// <summary>
+	/// Adds an in memory vault service to the service collection
+	/// </summary>
+	/// <param name="services">The service collection to add to the vault</param>
+	/// <param name="cacheEntryOptionsProvider">Configure the cache entry options <see cref="MemoryCacheOptions"/></param>
+	/// <param name="cacheOptionsProvider"></param>
+	/// <returns></returns>
+	public static IServiceCollection AddMoltenObsidianInMemoryVault(this IServiceCollection services, 
+		MemoryCacheEntryOptions cacheEntryOptionsProvider, 
+		MemoryCacheOptions cacheOptionsProvider, 
+		DirectoryInfo rootDirectoryInfo)
+	{
+		MemoryCache cache = new MemoryCache(cacheOptionsProvider);
+		return services.AddSingleton<IVault>(InMemoryVault.FromDirectory(cache, rootDirectoryInfo));
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="services"></param>
+	/// <returns></returns>
+	public static IServiceCollection AddMoltenObsidianInMemoryVault(this IServiceCollection services, DirectoryInfo rootDirectoryInfo)
+		=> services.AddMoltenObsidianInMemoryVault(DefaultEntryCacheOptions, DefaultCacheOptions, rootDirectoryInfo);
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="services"></param>
+	/// <param name="cacheEntryOptionsProvider"></param>
+	/// <param name="rootDirectoryInfo"></param>
+	/// <returns></returns>
+	public static IServiceCollection AddMoltenObsidianInMemoryVault(this IServiceCollection services, 
+		MemoryCacheEntryOptions cacheEntryOptionsProvider,
+        DirectoryInfo rootDirectoryInfo)
+		=> services.AddMoltenObsidianInMemoryVault(cacheEntryOptionsProvider, DefaultCacheOptions, rootDirectoryInfo);
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="services"></param>
+	/// <param name="cacheOptionsProvider"></param>
+	/// <param name="rootDirectoryInfo"></param>
+	/// <returns></returns>
+	public static IServiceCollection AddMoltenObsidianInMemoryVault(this IServiceCollection services, 
+		MemoryCacheOptions cacheOptionsProvider,
+		DirectoryInfo rootDirectoryInfo)
+		=> services.AddMoltenObsidianInMemoryVault(DefaultEntryCacheOptions, cacheOptionsProvider, rootDirectoryInfo);
+}
+
