959629c85c557ef09d0fa4fa39d20ee3f23c8245
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2023-11-16 20:43:45 +0100

    feat(deploy): Add workflow to deploy Astro website to GitHub Pages
    This commit adds a new workflow file `deploy-astro-website.yml` that triggers the deployment of the Astro website to GitHub Pages whenever changes are pushed to the `main` branch. The workflow includes steps for checking out the repository, setting up MoltenObsidian, generating Vault manifest and SSG assets, and installing, building, and uploading the site using Astro.
    
    Additionally, this commit updates the `astro.config.mjs` file with a new `site` property set to `'https://nodsoft.github.io/moltenobsidian/'` and a `base` property set to `'/moltenobsidian/'`. It also modifies the `BaseHead.astro` component by importing the Astro configuration from `astro.config.mjs`, extracting title and description from Astro props, and setting the base href dynamically based on the value of `{base}`.

--- nodsoft_moltenobsidian_web/astro.config.mjs
+++ nodsoft_moltenobsidian_web/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({    
+export default defineConfig({
+    site: 'https://nodsoft.github.io/moltenobsidian/',
+    base: '/moltenobsidian/',
     output: "static"
 });
