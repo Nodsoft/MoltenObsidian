55582fb0fa3df9ac60b6219411c49e8714912c3c
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2023-11-16 22:45:09 +0100

    feat: Update base URL in astro.config.mjs
    The base URL in the astro.config.mjs file has been updated from '/moltenobsidian/' to '/moltenobsidian'. This change ensures that the correct base URL is used for the website.

--- nodsoft_moltenobsidian_web/astro.config.mjs
+++ nodsoft_moltenobsidian_web/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://nodsoft.github.io/moltenobsidian/',
-    base: '/moltenobsidian/',
-    output: "static"
+    base: '/moltenobsidian',
+    output: 'static'
 });
