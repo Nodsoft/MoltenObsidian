caab8232a7d9fee5875dea38e33a788e72afa3e1
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2024-03-11 18:01:19 +0100

    feat(samples): Create WASM sample from demo website

---
+++ Samples/nodsoft_moltenobsidian_web_wasm/astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://nodsoft.github.io',
+    base: '/MoltenObsidian/',
+    output: 'static'
+});
