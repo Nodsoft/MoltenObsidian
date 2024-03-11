6149f32abaef6f6f5cdb96ba831fa5d4625e4e76
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2024-03-11 16:48:10 +0100

    feat(wasm): Register WASM Container components

--- Nodsoft.MoltenObsidian.WasmContainer/Program.cs
+++ Nodsoft.MoltenObsidian.WasmContainer/Program.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Nodsoft.MoltenObsidian.WasmContainer;
+using Nodsoft.MoltenObsidian.WasmContainer.Components;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.RegisterForJavaScript<RemoteVaultDisplay>(
+    identifier: "moltenobsidian-display-remote",
+    javaScriptInitializer: "initializeComponent"
+);
+
+builder.RootComponents.RegisterForJavaScript<RemoteVaultNavigation>(
+    identifier: "moltenobsidian-nav-remote",
+    javaScriptInitializer: "initializeComponent"
+);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
