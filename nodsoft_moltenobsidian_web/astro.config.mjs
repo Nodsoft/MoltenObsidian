954096618abac01379bd03e22fe348b56f64becf
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2024-06-22 14:10:58 +0200

    feat: Update site and base URLs
    The site URL has been updated to 'https://moltenobsidian.dev' and the base URL has been changed to '/'. This ensures that the website is correctly configured for deployment.

--- nodsoft_moltenobsidian_web/astro.config.mjs
+++ nodsoft_moltenobsidian_web/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nodsoft.github.io',
-    base: '/MoltenObsidian/',
+    site: 'https://moltenobsidian.dev',
+    base: '/',
     output: 'static'
 });
