64b9afcd705b286a9c89251155c94af9d438b789
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2024-06-23 11:33:53 +0200

    feat(config): Add sitemap integration and update Astro config
    - Added `@astrojs/sitemap` as a dependency in `package.json`
    - Updated `astro.config.mjs` to include the `integrations` property with the value of `[sitemap()]`
    - Added `<link rel="sitemap" href={base + "sitemap-index.xml"} />` in `BaseHead.astro`
    
    These changes enable sitemap generation for the website and improve SEO.

--- nodsoft_moltenobsidian_web/astro.config.mjs
+++ nodsoft_moltenobsidian_web/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://moltenobsidian.dev',
-    base: '/',
-    output: 'static'
+  site: 'https://moltenobsidian.dev',
+  base: '/',
+  output: 'static',
+  integrations: [sitemap()]
 });
