fa2433f978b388a971c28be963375353a5ee9805
LAPTOP-5VCKOCNQ\user <skfhddlg@vascoenergy.com>
2025-05-31 18:59:49 +0900

    Connect to a sample Github Pages Vault

--- Samples/Nodsoft.MoltenObsidian.Samples.HttpWasm/Program.cs
+++ Samples/Nodsoft.MoltenObsidian.Samples.HttpWasm/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Nodsoft.MoltenObsidian.Blazor;
 using Nodsoft.MoltenObsidian.Samples.HttpWasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,5 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddMoltenObsidianHttpAsyncVault("https://naratteu.github.io/daangn-garden/");
+builder.Services.AddMoltenObsidianBlazorIntegration();
 
 await builder.Build().RunAsync();
