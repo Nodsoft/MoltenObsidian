c1bcca763f0293f6f73b61cfe13c67a00fbab64d
나라뜨 <skfhddlg@naver.com>
2025-06-10 19:18:07 +0000

    docs: add XML comments to AsyncVaultDependencyInjectionExtensions

--- Vaults/Nodsoft.MoltenObsidian.Vaults.Http/VaultDependencyInjectionExtensions.cs
+++ Vaults/Nodsoft.MoltenObsidian.Vaults.Http/VaultDependencyInjectionExtensions.cs
@@ -41,8 +41,19 @@ public static class VaultDependencyInjectionExtensions
 		=> serviceCollection.AddMoltenObsidianHttpVault(_ => new() { BaseAddress = new(vaultRootUri) });
 }
 
+/// <summary>
+/// Provides extension methods for configuring asynchronous MoltenObsidian vaults in the dependency injection container.
+/// </summary>
+[PublicAPI]
 public static class AsyncVaultDependencyInjectionExtensions
 {
+	/// <summary>
+	/// Adds a HTTP-based MoltenObsidian vault asynchronously to the service collection.
+	/// </summary>
+	/// <param name="serviceCollection">The service collection to add the vault to.</param>
+	/// <param name="httpClientProvider">A function that returns an <see cref="HttpClient"/> to use for the vault.</param>
+	/// <returns>The service collection.</returns>
+	/// <exception cref="InvalidOperationException">Thrown if the <see cref="RemoteVaultManifest"/> could not be retrieved from the root vault address.</exception>
 	public static IServiceCollection AddMoltenObsidianHttpAsyncVault(this IServiceCollection serviceCollection, Func<IServiceProvider, HttpClient> httpClientProvider)
 		=> serviceCollection.AddSingleton<Task<IVault>>(async services =>
 		{
@@ -54,6 +65,17 @@ public static class AsyncVaultDependencyInjectionExtensions
 
 			return HttpRemoteVault.FromManifest(manifest, httpClient);
 		});
+
+	/// <summary>
+	/// Adds a HTTP-based MoltenObsidian vault asynchronously to the service collection.
+	/// </summary>
+	/// <remarks>
+	/// This overload provides its own <see cref="HttpClient"/> instance, which is configured to use the specified base address. <br />
+	/// In most cases, you should use <see cref="AddMoltenObsidianHttpVault(IServiceCollection,Func{IServiceProvider,HttpClient})"/> instead.
+	/// </remarks>
+	/// <param name="serviceCollection">The service collection to add the vault to.</param>
+	/// <param name="vaultRootUri">The root URI of the vault.</param>
+	/// <returns>The service collection.</returns>
 	public static IServiceCollection AddMoltenObsidianHttpAsyncVault(this IServiceCollection serviceCollection, string vaultRootUri)
 		=> serviceCollection.AddMoltenObsidianHttpAsyncVault(_ => new() { BaseAddress = new(vaultRootUri) });
 }
